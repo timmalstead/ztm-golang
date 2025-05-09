078265318edd34a8569ab3bf2b30ca9d7e2bc8b8
timmalstead <timmalstead@gmail.com>
2025-05-09 15:46:21 -0700

    adds functions lesson

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -19,7 +19,61 @@ package main
 
 import "fmt"
 
-func main() {
+//* Write a function that accepts a person's name as a function
+//  parameter and displays a greeting to that person.
 
+func sayMyName(name string) string {
+	return "Hello " + name + "!"
 }
 
+//* Write a function that returns any message, and call it from within
+//  fmt.Println()
+
+func saySomething() string {
+	return "Jet fuel can't melt steel beams"
+}
+
+//* Write a function to add 3 numbers together, supplied as arguments, and
+//  return the answer
+
+func addThreeNumbers(int1, int2, int3 int) int {
+	return int1 + int2 + int3
+}
+
+//* Write a function that returns any number
+
+func giveMeFive() int {
+	return 5
+}
+
+func returnThreeNumbers(int1, int2, int3 int) (int, int, int) {
+	return int1, int2, int3
+}
+
+//* Write a function that returns any two numbers
+
+func returnTwoNumbers(int1, int2 int) (int, int) {
+	return int1, int2
+}
+
+//* Add three numbers together using any combination of the existing functions.
+//  * Print the result
+//* Call every function at least once
+
+func main() {
+	fmt.Println(sayMyName("Tim"))
+	fmt.Println(saySomething())
+	fmt.Println(addThreeNumbers(1, 2, 3))
+
+	// ignoring third number
+	var num1, num2, _ int = returnThreeNumbers(4, 6, 9)
+	fmt.Println(num1, num2)
+
+	fmt.Println(giveMeFive())
+
+	var num3, num4 int = returnTwoNumbers(4, 6)
+	var sum int = num3 + num4 + giveMeFive()
+
+	fmt.Println(sum)
+
+}
