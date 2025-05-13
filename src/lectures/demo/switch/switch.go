c6ca122c091acfc1cd15083b568f3b81a5b7c794
timmalstead <timmalstead@gmail.com>
2025-05-12 17:58:13 -0700

    adds switch statement assignments

--- src/lectures/demo/switch/switch.go
+++ src/lectures/demo/switch/switch.go
@@ -14,5 +14,24 @@ const (
 
 func main() {
 
-}
+	switch p := price(); {
+	case p < 2:
+		fmt.Println("Cheap!")
+	case p < 10:
+		fmt.Println("less cheap!")
+	default:
+		fmt.Println("expensive")
+	}
 
+	var ticket int = Economy
+	switch ticket {
+	case Economy:
+		fmt.Println("Economy seating")
+	case Business:
+		fmt.Println("Business seating")
+	case FirstClass:
+		fmt.Println("Best seating")
+	default:
+		fmt.Println("Other seating")
+	}
+}
--- src/lectures/exercise/switch/switch.go
+++ src/lectures/exercise/switch/switch.go
@@ -14,5 +14,21 @@ package main
 import "fmt"
 
 func main() {
+	switch age := 115; {
+	//  - "newborn" when age is 0
+	case age == 0:
+		fmt.Println("Newborn")
+	//  - "toddler" when age is 1, 2, or 3
+	case age < 4:
+		fmt.Println("Toddler")
+	//  - "child" when age is 4 through 12
+	case age < 13:
+		fmt.Println("Child")
+	//  - "teenager" when age is 13 through 17
+	case age < 18:
+		fmt.Println("Teenager")
+	//  - "adult" when age is 18+
+	default:
+		fmt.Println("Adult")
+	}
 }
-
