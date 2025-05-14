d92cf988ee7cd7240597024e031824f05432ed13
timmalstead <timmalstead@gmail.com>
2025-05-13 17:01:45 -0700

    completes loop assignment

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	var sum int = 0
+	fmt.Println("sum:", sum)
 
+	// var i int = 1
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("sum:", sum)
+	}
+
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("sum:", sum)
+	}
 }
-
--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,5 +15,40 @@ package main
 import "fmt"
 
 func main() {
-}
+	for i := 1; i <= 50; i++ {
+		//* Print integers 1 to 50, except:
+		var valToPrint string
 
+		var divisibleByThree, divisibleByFive = i%3 == 0, i%5 == 0
+		//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+		if divisibleByThree && divisibleByFive {
+			valToPrint = "FizzBuzz"
+			//  - Print "Fizz" if the integer is divisible by 3
+		} else if divisibleByThree {
+			valToPrint = "Fizz"
+			//  - Print "Buzz" if the integer is divisible by 5
+		} else if divisibleByFive {
+			valToPrint = "Buzz"
+		} else {
+			valToPrint = fmt.Sprint(i)
+		}
+
+		fmt.Println(valToPrint)
+	}
+
+	for i := 1; i <= 50; i++ {
+		var valToPrint string
+
+		if i%3 == 0 {
+			valToPrint += "Fizz"
+		}
+		if i%5 == 0 {
+			valToPrint += "Buzz"
+		}
+		if valToPrint == "" {
+			valToPrint = fmt.Sprint(i)
+		}
+
+		fmt.Println(valToPrint)
+	}
+}
