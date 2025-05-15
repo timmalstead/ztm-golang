9e613a8cd2166b5754d9c12a2a3461f3b1579e02
timmalstead <timmalstead@gmail.com>
2025-05-14 17:00:07 -0700

    makes dice roller

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -23,8 +23,51 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func main() {
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
 }
 
+func main() {
+	// seed random number generator
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	//* The program must use variables to configure:
+	//  - number of times to roll the dice
+	//  - number of dice used in the rolls
+	//  - number of sides of all the dice (6-sided, 10-sided, etc determined
+	//    with a variable). All dice must use the same variable for number
+	//    of sides, so they all have the same number of sides.
+	var rolls, dice, sides int = 5, 3, 20
+
+	for r := 1; r <= rolls; r++ {
+		var sum int
+
+		for d := 1; d <= dice; d++ {
+			sum += roll(sides)
+		}
+
+		//* Print the sum of the dice roll
+		fmt.Println("Sum:", sum)
+		//* Print additional information in these circumstances:
+		if sum == 2 && dice == 2 {
+			//  - "Snake eyes": when the total roll is 2, and total dice is 2
+			fmt.Println("Snake eyes")
+		} else if sum == 7 {
+			//  - "Lucky 7": when the total roll is 7
+			fmt.Println("Lucky 7")
+		} else if sum%2 == 0 {
+			//  - "Even": when the total roll is even
+			fmt.Println("Even")
+		} else {
+			//  - "Odd": when the total roll is odd
+			fmt.Println("Odd")
+		}
+	}
+
+}
