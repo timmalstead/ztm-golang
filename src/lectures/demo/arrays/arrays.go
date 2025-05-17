d93e58513e484a80eeaff5be94f2b2762f081ffa
timmalstead <timmalstead@gmail.com>
2025-05-16 19:36:31 -0700

    array lesson, very interesting

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,7 +2,49 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Room struct {
+	name      string
+	isCleaned bool
 }
 
+// the argument of the array below has to be typed with the length of the array, it cannot be spread like creating an array can
+func checkIfClean(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		// good practice in go to cache the working element in a variable to not run into issues with concurrency
+		// do this EVEN if you need to work with the iterator value itself. if you need to work with the integer do something like `var iterator = i`
+		var currentRoom Room = rooms[i]
+		if currentRoom.isCleaned {
+			fmt.Println("That room is clean")
+		} else {
+			fmt.Println("That room is dirty")
+		}
+	}
+}
+
+func main() {
+	// all of these will be created with isCleaned as false, as that is the default value for booleans
+	var roomsToClean = [...]Room{
+		{name: "Antechamber"},
+		{name: "Foyer"},
+		{name: "Solarium"},
+		{name: "Veranda"},
+	}
+	checkIfClean(roomsToClean)
+
+	fmt.Println("\ngetting to it...\n")
+
+	roomsToClean[0].isCleaned = true
+	roomsToClean[2].isCleaned = true
+
+	// since this creates a COPY of the data, changing foyer.isCleaned will not effect the value of the array element it came from
+	var foyer = roomsToClean[1]
+	foyer.isCleaned = true
+
+	checkIfClean(roomsToClean)
+
+	fmt.Println("\none more time\n")
+
+	roomsToClean[1].isCleaned = true
+
+	checkIfClean(roomsToClean)
+}
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,7 +18,48 @@ package main
 
 import "fmt"
 
-func main() {
-
+type GroceryItem struct {
+	productName string
+	productCost int
 }
 
+// * Print to the terminal:
+//   - The last item on the list
+//   - The total number of items
+//   - The total cost of the items
+func findListInformation(list [4]GroceryItem) {
+	var (
+		// lastItem                             string
+		totalNumberOfItems, totalCostOfItems int
+	)
+
+	for i := 0; i < len(list); i++ {
+		var currentListItem = list[i]
+
+		if currentListItem.productName != "" {
+			totalNumberOfItems++
+			totalCostOfItems += currentListItem.productCost
+			// lastItem = currentListItem.productName
+		}
+	}
+
+	// fmt.Println("Last item on list:", lastItem, ". Total number of items:", totalNumberOfItems, ". Total cost of items:", totalCostOfItems)
+	fmt.Println("Last item on list:", list[totalNumberOfItems-1], ". Total number of items:", totalNumberOfItems, ". Total cost of items:", totalCostOfItems)
+}
+
+func main() {
+	var shoppingList = [4]GroceryItem{
+		{productName: "cereal", productCost: 2},
+		{productName: "coffee", productCost: 1},
+		{productName: "orange juice", productCost: 2},
+	}
+
+	// length is the total length of the array, even if there is no element explicitly put there
+	fmt.Println(len(shoppingList), shoppingList[len(shoppingList)-1].productName == "")
+	findListInformation(shoppingList)
+
+	// Guess you can't add to an array created using the spread operator. Makes sense I guess, because the size has to be known when instantiated
+	shoppingList[3] = GroceryItem{"milk", 3}
+
+	findListInformation(shoppingList)
+}
