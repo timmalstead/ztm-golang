6f7c39f3c43de63968be265bdcbeacf14c4a59af
timmalstead <timmalstead@gmail.com>
2025-05-16 17:30:43 -0700

    struct lesson

--- src/lectures/demo/structs/structs.go
+++ src/lectures/demo/structs/structs.go
@@ -2,7 +2,47 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	IsBoarded    bool
 }
 
+type Bus struct {
+	FrontSeat Passenger
+}
+
+func main() {
+	var casey = Passenger{Name: "Casey", IsBoarded: false, TicketNumber: 1}
+
+	var (
+		bill = Passenger{"Bill", 2, false}
+		ella = Passenger{"Ella", 3, false}
+	)
+	fmt.Println(casey, bill, ella)
+
+	var heidi Passenger
+	heidi.Name = "Heidi"
+	heidi.TicketNumber = 4
+
+	fmt.Println(heidi)
+
+	casey.IsBoarded = true
+	bill.IsBoarded = true
+
+	if bill.IsBoarded {
+		fmt.Println("Bill has done it!")
+	} else {
+		fmt.Println("Bill can't get on!")
+	}
+
+	if casey.IsBoarded {
+		fmt.Println(casey.Name, "has boarded the bus")
+	}
+
+	heidi.IsBoarded = true
+
+	var crossTownExpress = Bus{FrontSeat: heidi}
+
+	fmt.Println(crossTownExpress.FrontSeat.Name, "in the front seat, party in the back")
+}
--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -19,7 +19,39 @@ package main
 
 import "fmt"
 
-func main() {
-
+// * Create a rectangle structure containing a length and width field
+type Rectangle struct {
+	Length int
+	Width  int
 }
 
+//* The area of a rectangle is length*width
+//* The perimeter of a rectangle is the sum of the lengths of all sides
+
+func findArea(rec Rectangle) int {
+	return rec.Length * rec.Width
+}
+
+func findPerimeter(rec Rectangle) int {
+	return (rec.Length + rec.Width) * 2
+}
+
+func main() {
+	//* Using functions, calculate the area and perimeter of a rectangle,
+	//  - Print the results to the terminal
+	//  - The functions must use the rectangle structure as the function parameter
+	var rectangle = Rectangle{Length: 2, Width: 2}
+	var initRecArea, initRecPerimeter = findArea(rectangle), findPerimeter(rectangle)
+
+	fmt.Println("starting rectangle area", initRecArea, "starting rectangle perimeter", initRecPerimeter)
+
+	//* After performing the above requirements, double the size
+	//  of the existing rectangle and repeat the calculations
+	//  - Print the new results to the terminal
+	rectangle.Length *= 2
+	rectangle.Width *= 2
+
+	var modifiedRecArea, modifiedRecPerimeter = findArea(rectangle), findPerimeter(rectangle)
+
+	fmt.Println("modified rectangle area", modifiedRecArea, "modified rectangle perimeter", modifiedRecPerimeter)
+}
