ec77650749f65c42a2281eec231a27b21e2810ff
timmalstead <timmalstead@gmail.com>
2025-05-18 18:23:49 -0700

    range lesson

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-}
+	fmt.Println("ranges")
+	var exampleSlice = []string{"hello", "world", "!"}
 
+	// range returns TWO things, the index and the value
+	for i, element := range exampleSlice {
+		fmt.Println(i, element)
+		for _, char := range element {
+			// convert numeric rune value to character
+			var charRuneConvertedToGlyph = fmt.Sprintf("%q", char)
+			fmt.Print(charRuneConvertedToGlyph)
+			// fmt.Printf("%q", char)
+		}
+		fmt.Println()
+	}
+}
