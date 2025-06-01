7ab41cd2e9f610401fbdd2c571380525f89fb084
timmalstead <timmalstead@gmail.com>
2025-05-31 17:53:56 -0700

    pointer lesson

--- src/lectures/demo/pointers/pointers.go
+++ src/lectures/demo/pointers/pointers.go
@@ -2,7 +2,48 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Counter struct {
+	hits int
 }
 
+func increment(countner *Counter) {
+	// seems good to know. with a REGULAR variable we would need to dereference with *, but using dot notation with a struct, we do not need to do that. I suspect this is because it is also a pointer
+	countner.hits++
+	fmt.Println("Counter", countner)
+}
+
+func replaceStr(old *string, new string, counter *Counter) {
+	// remember to dereference!
+	*old = new
+	increment(counter)
+}
+
+// by default, go uses "pass by value" when calling functions, meaning a COPY of information supplied as args. This can get quite unwieldy and it can be difficult to manage state, as not all copies of data may be the same. This can be helped by using pointers, which point to the original data in memory.
+func main() {
+	fmt.Println("Pointers")
+
+	var counter = Counter{}
+	fmt.Println(counter)
+
+	var hello = "Hello"
+	var world = "world!"
+
+	fmt.Println(hello, world)
+
+	replaceStr(&world, "there!", &counter)
+
+	fmt.Println(hello, world)
+	fmt.Println(counter)
+
+	// making a copy of the data in a new slice. I predict keeping track of what is a copy and what is not will be tricky at first
+	var phrase = []string{hello, world}
+	fmt.Println(phrase)
+
+	replaceStr(&phrase[1], "Go!", &counter)
+
+	// note that this printing will NOT Have hello go! because we created a COPY when we put these variables into a slice
+	fmt.Println(hello, world)
+	fmt.Println(phrase)
+
+	fmt.Println(counter)
+}
--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -18,7 +18,59 @@ package main
 
 import "fmt"
 
-func main() {
+// - Security tags have two states: active (true) and inactive (false)
+const (
+	Active   = true
+	Inactive = false
+)
 
+// * Create functions to activate and deactivate security tags using pointers
+
+func activateTag(tag *bool) {
+	*tag = Active
 }
 
+func deactivateTag(tag *bool) {
+	*tag = Inactive
+}
+
+//* Create a checkout() function which can deactivate all tags in a slice
+
+func checkout(tags []*bool) {
+	for _, tag := range tags {
+		deactivateTag(tag)
+	}
+}
+
+func main() {
+	fmt.Println("Pointers exercise")
+
+	//  - Create at least 4 items, all with active security tags
+	var timTag, sophiaTag, bobTag, billTag bool
+
+	activateTag(&timTag)
+	activateTag(&sophiaTag)
+	activateTag(&bobTag)
+	activateTag(&billTag)
+
+	//  - Store them in a slice or array
+	var tagHolder = []bool{timTag, sophiaTag, bobTag, billTag}
+
+	fmt.Println("original tags:", timTag, sophiaTag, bobTag, billTag)
+	fmt.Println("tags copied into slice:", tagHolder)
+
+	//  - Deactivate any one security tag in the array/slice
+
+	deactivateTag(&tagHolder[2])
+	deactivateTag(&tagHolder[3])
+
+	fmt.Println("original tags:", timTag, sophiaTag, bobTag, billTag)
+	fmt.Println("tags copied into slice:", tagHolder)
+	//  - Call the checkout() function to deactivate all tags
+	checkout([]*bool{&tagHolder[0], &tagHolder[1], &tagHolder[2], &tagHolder[3]})
+	//  - Print out the array/slice after each change
+
+	fmt.Println("original tags:", timTag, sophiaTag, bobTag, billTag)
+	fmt.Println("tags copied into slice:", tagHolder)
+
+}
