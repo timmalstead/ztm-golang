d4cfcc92c23b31c57df296e059ad4088ff5a7e0d
timmalstead <timmalstead@gmail.com>
2025-06-04 20:41:00 -0700

    completes receiver function assignment

--- src/lectures/demo/rcv-func/rcv-func.go
+++ src/lectures/demo/rcv-func/rcv-func.go
@@ -2,7 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+var p = fmt.Println
 
+type Space struct {
+	isOccupied bool
 }
 
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].isOccupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].isOccupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].isOccupied = false
+}
+
+func main() {
+	var lot = ParkingLot{spaces: make([]Space, 10)}
+	p(lot)
+	lot.occupySpace(1)
+	occupySpace(&lot, 2)
+	p(lot)
+	lot.vacateSpace(1)
+	p(lot)
+}
--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -14,9 +14,49 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
+// builtins are a good thing
 
+var p = fmt.Println
+
+type Player struct {
+	name                                 string
+	health, maxHealth, energy, maxEnergy int
 }
 
+func (player *Player) heal(healBy int) {
+	player.health = min(player.health+healBy, player.maxHealth)
+}
+
+func (player *Player) damage(hurtBy int) {
+	player.health = max(player.health-hurtBy, 0)
+}
+
+func (player *Player) powerUp(powerUpBy int) {
+	player.energy = min(player.energy+powerUpBy, player.maxEnergy)
+}
+
+func (player *Player) blast(consume int) {
+	player.energy = max(player.energy-consume, 0)
+}
+
+func main() {
+	var playerOne = Player{name: "Timothy", health: 100, maxHealth: 1000, energy: 100, maxEnergy: 1000}
+	p(playerOne)
+
+	playerOne.heal(100)
+	playerOne.powerUp(100)
+
+	p(playerOne)
+
+	playerOne.damage(199)
+	p(playerOne)
+
+	playerOne.heal(10000)
+	playerOne.blast(9999)
+	p(playerOne)
+
+}
