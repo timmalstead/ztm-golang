429170ed5ff9a34ce238dd6f6f3cb2ddb79c1d9d
timmalstead <timmalstead@gmail.com>
2025-06-07 18:28:05 -0700

    completes iota exercise

--- src/lectures/exercise/iota/iota.go
+++ src/lectures/exercise/iota/iota.go
@@ -16,6 +16,51 @@ package main
 
 import "fmt"
 
+// lost ten minutes when i forgot that you do NOT use the = operand to assign for a type alias
+type Operation int
+
+const (
+	add Operation = iota
+	sub
+	mul
+	div
+)
+
+var calculations = map[Operation]func(int, int) int{
+	add: func(op1, op2 int) int {
+		return op1 + op2
+	},
+	sub: func(op1, op2 int) int {
+		return op1 - op2
+	},
+	mul: func(op1, op2 int) int {
+		return op1 * op2
+	},
+	div: func(op1, op2 int) int {
+		return op1 / op2
+	},
+}
+
+func (op Operation) calculate(op1, op2 int) int {
+	var opToExecute, opExists = calculations[op]
+	if opExists {
+		return opToExecute(op1, op2)
+	}
+	panic("invalid operation!")
+}
+
+// func (op Operation) calculate(op1, op2 int) int {
+// 	if op == add {
+// 		return op1 + op2
+// 	} else if op == sub {
+// 		return op1 - op2
+// 	} else if op == mul {
+// 		return op1 * op2
+// 	} else {
+// 		return op1 / op2
+// 	}
+// }
+
 func main() {
 	fmt.Println(add.calculate(2, 2)) // = 4
 
@@ -25,4 +70,3 @@ func main() {
 
 	fmt.Println(div.calculate(100, 2)) // = 50
 }
-
