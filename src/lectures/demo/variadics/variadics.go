061332301cacf7c2a295086ce31ff53d364809ee
timmalstead <timmalstead@gmail.com>
2025-06-07 18:46:42 -0700

    completes variadics exercise

--- src/lectures/demo/variadics/variadics.go
+++ src/lectures/demo/variadics/variadics.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-
+func sum(nums ...int) int {
+	var finalSum int
+	for _, n := range nums {
+		finalSum += n
+	}
+	return finalSum
 }
 
+func main() {
+	fmt.Println("variadics!")
+	// like the spread param in js, variadics are a way to write a function that accepts any number of params
+	var (
+		slice1 = []int{1, 2, 3}
+		slice2 = []int{4, 5, 6}
+	)
+
+	// remember, the spread in go is AFTER the value to be spread
+	fmt.Println(sum(slice1...))
+	fmt.Println(sum(slice2...))
+
+	var compoundSlice = append(slice1, slice2...)
+	fmt.Println(sum(compoundSlice...))
+}
