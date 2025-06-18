1777253e415b42246679487b733d9a9572e30e5e
timmalstead <timmalstead@gmail.com>
2025-06-17 21:30:32 -0700

    completes interface lesson

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,7 +2,41 @@ package main
 
 import "fmt"
 
-func main() {
+var p = fmt.Println
 
+type Preparer interface {
+	PrepareDish()
 }
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	p("cook that chicken")
+}
+
+func (c Salad) PrepareDish() {
+	p("chop salad")
+	p("add dressing")
+}
+
+func prepareDishes(dishes []Preparer) {
+	p("preparing dishes")
+	for _, dish := range dishes {
+		fmt.Printf("Dish: %v\n", dish)
+
+		// okay, can check for different types here, but I'm not exactly sure what I'm doing
+		var _, isChicken = dish.(Chicken)
+		p(isChicken)
+
+		dish.PrepareDish()
+	}
+}
+
+func main() {
+	var chickenDish Chicken = "Chicken Pot Pie"
+	var saladDish Salad = "Endive Salad"
+
+	prepareDishes([]Preparer{chickenDish, saladDish})
+
+}
--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -21,7 +21,90 @@ package main
 
 import "fmt"
 
-func main() {
+var p, f = fmt.Println, fmt.Printf
 
+type Vehicle struct {
+	model string
 }
 
+type Motorcycle Vehicle
+type Car Vehicle
+type Truck Vehicle
+
+type Lifter interface {
+	Lift()
+}
+
+func (v Motorcycle) Lift() {
+	f("small lift for motorcycle %v\n", v.model)
+}
+
+func (v Car) Lift() {
+	f("medium lift for car %v\n", v.model)
+}
+
+func (v Truck) Lift() {
+	f("large lift for truck %v\n", v.model)
+}
+
+func directLifters(liftables []Lifter) {
+	p("liftin' all we can")
+
+	for _, vehicle := range liftables {
+		vehicle.Lift()
+	}
+}
+
+type IntAlias int
+type StrAlias string
+
+type AliasChecker interface {
+	CheckType()
+}
+
+func (i IntAlias) CheckType() {
+	f("that's an int: %v\n", i)
+}
+
+func (s StrAlias) CheckType() {
+	f("that's a string: %v\n", s)
+}
+
+func typeChecker(types []AliasChecker) {
+	for _, typeAlias := range types {
+		typeAlias.CheckType()
+		var _, isInteger = typeAlias.(IntAlias)
+		var _, isIntegerPointer = typeAlias.(*IntAlias)
+
+		if isInteger {
+			p("I told you that was an integer!")
+		}
+		if isIntegerPointer {
+			p("I told you that was an integer pointer!")
+		}
+
+		var _, isString = typeAlias.(StrAlias)
+		var _, isStringPointer = typeAlias.(*StrAlias)
+
+		if isString {
+			p("I told you that was a string!")
+		}
+		if isStringPointer {
+			p("I told you that was a string pointer!")
+		}
+
+	}
+}
+
+func main() {
+	var shark = Motorcycle{"Swifty Shark"}
+	var cuda = Car{"Cunning 'Cuda"}
+	var hulk = Truck{"Happy Hulk"}
+
+	// use pointers or copies
+	directLifters([]Lifter{&shark, &cuda, hulk})
+
+	var intExample, strExample = IntAlias(5), StrAlias("hello")
+
+	typeChecker([]AliasChecker{&intExample, strExample})
+}
