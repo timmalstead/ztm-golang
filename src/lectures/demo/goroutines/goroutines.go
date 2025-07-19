9aae8ba9bfdfb1afe926e881919140474f203195
timmalstead <timmalstead@gmail.com>
2025-07-18 17:54:21 -0700

    lesson on goroutines

--- src/lectures/demo/goroutines/goroutines.go
+++ src/lectures/demo/goroutines/goroutines.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
 
 func main() {
 	data := []rune{'a', 'b', 'c', 'd'}
-}
+	var capitalized []rune
 
+	var capIt = func(r rune) {
+		capitalized = append(capitalized, unicode.ToUpper((r)))
+		fmt.Printf("%c done!\n", r)
+	}
+
+	for _, letter := range data {
+		go capIt(letter)
+	}
+
+	// for i := 0; i < len(data); i++ {
+	// 	go capIt(data[i])
+	// }
+
+	fmt.Println("Capitalized 1:", capitalized)
+	time.Sleep(200 * time.Millisecond)
+	fmt.Printf("Capitalized 2: %c\n", capitalized)
+}
--- src/lectures/exercise/goroutines/goroutines.go
+++ src/lectures/exercise/goroutines/goroutines.go
@@ -31,10 +31,67 @@ import (
 	"io"
 	"os"
 	"strconv"
-	"time"
+	"strings"
+	// "time"
 )
 
 func main() {
-	files := []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
-}
+	var files = []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
+	var grandTotal, filesProcessed int
 
+	var incrementFilesProcessed = func() {
+		filesProcessed++
+	}
+
+	var processNumberFile = func(filePath string) {
+		var expandedPath = fmt.Sprintf("./exercise/goroutines/%v", filePath)
+		var file, fileOpenErr = os.Open(expandedPath)
+		defer file.Close()
+		// this way it will increment the file processing counter whether it is successful or not
+		defer incrementFilesProcessed()
+
+		if fileOpenErr != nil {
+			fmt.Println("Error opening file: ", fileOpenErr)
+		} else {
+			var fileTotal int
+
+			var fileReader = bufio.NewReader(file)
+			for {
+				var line, lineErr = fileReader.ReadString('\n')
+
+				if lineErr == io.EOF {
+					break
+				}
+
+				var trimmedLine = strings.TrimSpace(line)
+				var num, convertErr = strconv.Atoi(trimmedLine)
+
+				if convertErr == nil {
+					fileTotal += num
+				}
+
+			}
+
+			grandTotal += fileTotal
+			// filesProcessed++
+		}
+	}
+
+	// this makes sense as a candidate to be parallelized as we don't care about the order of the calculations, just the total sum
+	for _, file := range files {
+		go processNumberFile(file)
+	}
+
+	fmt.Println(grandTotal)
+	// time.Sleep(100 * time.Millisecond)
+	//* The grand total for the files is 4103109
+
+	// guess you can wait on the parallel call with another loop. makes sense
+	// there would probably need to be something else though, like a way to terminate if it goes over a timeout
+	// I hope I learn about that in future lessons
+	for filesProcessed < len(files) {
+		fmt.Print(".")
+	}
+
+	fmt.Println("\n", grandTotal, grandTotal == 4103109)
+}
