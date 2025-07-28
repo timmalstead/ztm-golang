16f90879698d6b63b1813a8e47b44a71e0217c54
timmalstead <timmalstead@gmail.com>
2025-07-27 19:12:08 -0700

    lesson about wait groups

--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
-}
+	var waitGroup sync.WaitGroup
+	var counter int
 
+	for i := 0; i < 5; i++ {
+		waitGroup.Add(1)
+		counter++
+
+		go func() {
+			defer func() {
+				fmt.Println(counter, "goroutines remaining")
+				counter--
+				waitGroup.Done()
+			}()
+			var duration = time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Println("waiting for", duration)
+			time.Sleep(duration)
+		}()
+	}
+
+	fmt.Println("waiting for goroutines")
+	waitGroup.Wait()
+	fmt.Println("complete")
+}
