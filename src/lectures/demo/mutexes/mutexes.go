506c9067bf2ed1b1862176836526c113f1c38458
timmalstead <timmalstead@gmail.com>
2025-07-28 18:30:03 -0700

    lesson about mutexes and synchronization techniques

--- src/lectures/demo/mutexes/mutexes.go
+++ src/lectures/demo/mutexes/mutexes.go
@@ -3,14 +3,46 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
+
+type Hits struct {
+	count int
+	mutex sync.Mutex
+}
 
 func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 }
 
-func main() {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
+func serve(waitGroup *sync.WaitGroup, hitCounter *Hits, iteration int) {
+	wait()
+	hitCounter.mutex.Lock()
+	defer hitCounter.mutex.Unlock()
+	defer waitGroup.Done()
+	hitCounter.count++
+	fmt.Println("server iteration", iteration)
 }
 
+func main() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var waitGroup sync.WaitGroup
+	var hitCounter = Hits{}
+
+	var amountOfHitsToSimulate = 20
+	waitGroup.Add(amountOfHitsToSimulate)
+
+	for i := 0; i < amountOfHitsToSimulate; i++ {
+		var currentIteration = i
+		go serve(&waitGroup, &hitCounter, currentIteration)
+	}
+	fmt.Printf("waiting for goroutines....\n\n")
+	waitGroup.Wait()
+
+	hitCounter.mutex.Lock()
+	var totalHits = hitCounter.count
+	hitCounter.mutex.Unlock()
+	fmt.Println("total hits", totalHits)
+}
--- src/lectures/exercise/sync/sync.go
+++ src/lectures/exercise/sync/sync.go
@@ -19,8 +19,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
 )
 
-func main() {}
+func main() {
+	//* Count the total number of letters in any chosen input
+	var totalLetterCount int
+	//* The input must be supplied from standard input
+	var stdInReader = bufio.NewReader(os.Stdin)
 
+	var separatedStrings = []string{}
+	// I don't THINK that there is a way to do this reading in goroutines
+	// as I understand it the nature of the reader is getting some bytes, consuming them and then waiting for more to work with
+	// so MAYBE we could do it in a goroutine if I knew the sizes of the lines beforehand, but I'm not going to try that
+	for {
+		var input, inputErr = stdInReader.ReadString('\n')
+		if inputErr == io.EOF {
+			break
+		}
+		var trimmedInput = strings.TrimSpace(input)
+		var splitInput = strings.Split(trimmedInput, " ")
+		separatedStrings = append(separatedStrings, splitInput...)
+	}
+	//* Input analysis must occur per-word, and each word must be analyzed
+	//  within a goroutine
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(separatedStrings))
+
+	var countLetters = func(word string) {
+		defer waitGroup.Done()
+		totalLetterCount += len(word)
+	}
+
+	// didn't really see any need to use a mutex on this one
+	for _, word := range separatedStrings {
+		go countLetters(word)
+	}
+
+	waitGroup.Wait()
+
+	//* When the program finishes, display the total number of letters counted
+	// should be 29 letters counted
+	fmt.Println("totalLetterCount", totalLetterCount)
+}
