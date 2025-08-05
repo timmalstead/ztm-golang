0a7930edfff17f876904fed6a68aa517278ecaea
timmalstead <timmalstead@gmail.com>
2025-08-04 19:46:43 -0700

    adds channel based solution for mgrep that almost works

--- src/projects/mgrep/mgrep.go
+++ src/projects/mgrep/mgrep.go
@@ -118,3 +118,145 @@ func main() {
 		fmt.Printf("No matches for %v were found in %v or any of its subdirectories\n", searchString, absolutePath)
 	}
 }
+
+////////////////
+////////////////
+////////////////
+////////////////
+
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io"
+// 	"log"
+// 	"os"
+// 	"path/filepath"
+// 	"strings"
+// 	"sync"
+// )
+
+// const BUFFER_SIZE = 100
+
+// func validateArgs(grepArgs []string) {
+// 	if len(grepArgs) != 2 {
+// 		log.Fatal("\nmgrep requires two arguments in an exact order\n1) <search_string>: The string to search for.\n2) <search_dir>: The directory in which to search.")
+// 	}
+// }
+
+// func validateSearchDirectory(searchDir string) string {
+// 	var directoryError = os.Chdir(searchDir)
+// 	if directoryError != nil {
+// 		log.Fatal(directoryError)
+// 	}
+
+// 	var absolutePathDir, pathErr = os.Getwd()
+// 	if pathErr != nil {
+// 		log.Fatal(pathErr)
+// 	}
+
+// 	fmt.Printf("%v is a valid directory\n", absolutePathDir)
+// 	return absolutePathDir
+// }
+
+// func recurseDirectories(dir string, filesChannel chan string) {
+// 	var entries, dirErr = os.ReadDir(dir)
+// 	if dirErr != nil {
+// 		log.Fatal(dirErr)
+// 	}
+
+// 	var waitGroup sync.WaitGroup
+// 	waitGroup.Add(1)
+// 	go func() {
+// 		defer waitGroup.Done()
+// 		for _, entry := range entries {
+// 			var entryName = filepath.Join(dir, entry.Name())
+// 			if entry.IsDir() {
+// 				recurseDirectories(entryName, filesChannel)
+// 			} else {
+// 				filesChannel <- entryName
+// 			}
+// 		}
+// 	}()
+// 	waitGroup.Wait()
+// }
+
+// func getFiles(dir string, filesChannel chan string) {
+// 	var waitGroup sync.WaitGroup
+// 	waitGroup.Add(1)
+// 	go func() {
+// 		defer waitGroup.Done()
+// 		recurseDirectories(dir, filesChannel)
+// 	}()
+// 	waitGroup.Wait()
+// 	close(filesChannel)
+// }
+
+// func grepFiles(searchString string, filesChannel, matchesChannel chan string) {
+// 	var waitGroup sync.WaitGroup
+
+// 	for path := range filesChannel {
+// 		go func() {
+// 			defer waitGroup.Done()
+// 			waitGroup.Add(1)
+
+// 			var file, fileOpenErr = os.Open(path)
+// 			defer file.Close()
+
+// 			if fileOpenErr != nil {
+// 				log.Fatal(fileOpenErr)
+// 			}
+
+// 			var fileReader = bufio.NewReader(file)
+// 			var currentLine = 1
+// 			for {
+// 				var line, lineErr = fileReader.ReadString('\n')
+
+// 				var lineHasSearchString = strings.Contains(line, searchString)
+
+// 				if lineHasSearchString {
+// 					var notification = fmt.Sprintf("---\nSearch string: %v\nFile path: %v, Line number: %v\nMatching line: %v\n", searchString, path, currentLine, line)
+// 					matchesChannel <- notification
+// 				}
+
+// 				currentLine++
+// 				if lineErr == io.EOF {
+// 					break
+// 				}
+// 			}
+// 		}()
+// 	}
+// 	waitGroup.Wait()
+// 	close(matchesChannel)
+// }
+
+// func announceResults(searchString, absolutePath string, matchesChannel chan string) {
+// 	var matchesFound = false
+
+// 	for match := range matchesChannel {
+// 		matchesFound = true
+// 		fmt.Println(match)
+// 	}
+
+// 	if !matchesFound {
+// 		fmt.Printf("No matches for %v were found in %v or any of its subdirectories\n", searchString, absolutePath)
+// 	}
+// }
+
+// func main() {
+// 	var grepArgs = os.Args[1:]
+// 	validateArgs(grepArgs)
+
+// 	var searchString = grepArgs[0]
+// 	var searchDir = grepArgs[1]
+// 	var absolutePath = validateSearchDirectory(searchDir)
+
+// 	var filesChannel = make(chan string, BUFFER_SIZE)
+// 	getFiles(absolutePath, filesChannel)
+
+// 	var matchesChannel = make(chan string, BUFFER_SIZE)
+// 	grepFiles(searchString, filesChannel, matchesChannel)
+
+// 	announceResults(searchString, absolutePath, matchesChannel)
+// }
